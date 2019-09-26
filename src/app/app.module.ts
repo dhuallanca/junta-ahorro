cb170c2b1fa6fc9980601ff8b54caa6feb0440a5
unknown <dzavaleta@hotmail.com>
2019-09-26 11:25:28 -0500

    build(angular-json): use scss for new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GroupComponent } from './group/group.component';
+import { CreateComponent } from './group/create/create.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
+    CreateComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
